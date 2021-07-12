33d3991bcaf9d30a89fbe3697a2ed4edbda7c58b
rezasalehi <rezasalehidev@gmail.com>
2021-07-12 11:59:39 +0430

    Updates

---
+++ precache-manifest.2b90352d9958a009c74105e9c00ddc7a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "debb1f7b77dac6dea029897d61d6c8e4",
+    "url": "/tours-React/index.html"
+  },
+  {
+    "revision": "27235d77498a4e9864f2",
+    "url": "/tours-React/static/css/2.da8127bb.chunk.css"
+  },
+  {
+    "revision": "62878936baea221f2e54",
+    "url": "/tours-React/static/css/main.8e49c9d7.chunk.css"
+  },
+  {
+    "revision": "27235d77498a4e9864f2",
+    "url": "/tours-React/static/js/2.41e953c2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tours-React/static/js/2.41e953c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62878936baea221f2e54",
+    "url": "/tours-React/static/js/main.a43ac397.chunk.js"
+  },
+  {
+    "revision": "5198956880c99f1a6787",
+    "url": "/tours-React/static/js/runtime-main.1ffbff73.js"
+  }
+]);
